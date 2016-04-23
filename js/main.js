8d8e6ac2b227a6005e97684677f2259b52d89a5c
Kai, Lu <kailu@gatech.edu>
2016-04-23 17:06:02 -0400

    new ajax code

--- js/main.js
+++ js/main.js
@@ -757,6 +757,27 @@
 	// 	});
 	// }
 	var searchForDetail = function(commutingStyle,commutingTime,floorPlan,priceRange,propertyType,lifeCon){
+		console.log("create post is working!") // sanity check
+    	$.ajax({
+	        url : "gtrent", // the endpoint
+	        type : "GET", // http method
+	        data : "ID=16", // data sent with the post request
+
+	        // handle a successful response
+	        success : function(json) {
+	            //$('#post-text').val(''); // remove the value from the input
+	            //console.log(json); // log the returned json to the console
+	            console.log("success"); // another sanity check
+	        },
+
+	        // handle a non-successful response
+	        error : function(xhr,errmsg,err) {
+	            //$('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+	            //    " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+	            //console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+    	/*
 		var maps={};
 		var xhttp = new XMLHttpRequest();
   		xhttp.onreadystatechange = function() {
@@ -770,6 +791,7 @@
 	  	xhttp.send("id=1");
 
 		return maps
+		*/
 	}
 	var hideContent=function(){
 
@@ -802,6 +824,7 @@
 		filterToggle();
 		commutingStyle();
 		selectDetailToggle();
+		updateDetails();
 		// applyFilter();
 		//resumeContent();
 		$("#hideContent").click(function(){
