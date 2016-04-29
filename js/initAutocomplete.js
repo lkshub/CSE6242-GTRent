3ca2d91cf73064c0ba01dd19424022a5cd143832
Kai, Lu <kailu@gatech.edu>
2016-04-29 17:04:43 -0400

    set up my own team block; change about us text; hide the sign up button

--- js/initAutocomplete.js
+++ js/initAutocomplete.js
@@ -450,7 +450,7 @@ var showDetailWin=function(contentid){
       indexChange.push(index);
     }
   })
-  var detailDiv = $("<div>").css({"padding-bottom":"20px","position":"absolute","top":"20px","left":"0px","width":"400px","height":"95%","overflow-y":"scroll","overflow-x":"hidden","word-wrap": "break-word" ,"word-break": "normal" });
+  var detailDiv = $("<div>").css({"padding-left":"20px","padding-bottom":"20px","padding-top":"10px","position":"absolute","top":"20px","left":"0px","width":"400px","height":"95%","overflow-y":"scroll","overflow-x":"hidden","word-wrap": "break-word" ,"word-break": "normal" });
   detailDiv.append(header);
   addContent(recommendDiv,recommendData,indexChange);
   var address1 =$('<h3>').text(data["address"]+", "+data["zipCode"]);
