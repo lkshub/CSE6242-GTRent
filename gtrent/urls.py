34d0885f69274d6e707f21f9cc69690f9fd5367a
Kai, Lu <lk930121@gmail.com>
2017-01-14 20:34:49 -0500

    trying to fix bugs

--- gtrent/urls.py
+++ gtrent/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/filter$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^/nearby$', views.nearby, name = 'nearby')
 ]
